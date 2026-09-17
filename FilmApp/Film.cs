6c3b729e3e5c8b7fdb37caf111d9462cd555f5e7
MolnarMarcell <marci20070208@gmail.com>
2026-09-17 08:17:26 +0200

    1. Feladat

---
+++ FilmApp/Film.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FilmApp
+{
+    internal class Film
+    {
+        public string Cim;
+        public int HosszPercben;
+
+        public Film(string cim, int hosszpercben)
+        {
+            this.Cim = cim;
+            this.HosszPercben = hosszpercben;
+        }
+    }
+}
---
+++ FilmApp/Program.cs
@@ -0,0 +1,8 @@
+﻿using FilmApp;
+
+Film odusszeia = new Film("Odüsszeia", 240);
+
+Film pokember = new Film("Pókember - Brand new Day", 180);
+
+Console.WriteLine(odusszeia.Cim + "- " +  odusszeia.HosszPercben);
+Console.WriteLine(pokember.Cim + "- "+ pokember.HosszPercben);
