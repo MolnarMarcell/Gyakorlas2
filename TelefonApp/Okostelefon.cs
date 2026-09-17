b37612042dd72e89c9f50221ab96095afb3c6be3
MolnarMarcell <marci20070208@gmail.com>
2026-09-17 08:45:32 +0200

    4. feladat

---
+++ TelefonApp/Okostelefon.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TelefonApp
+{
+    internal class Okostelefon
+    {
+        public string Modell { get; set; }
+
+        private int toltottseg;
+
+        public int Toltottseg
+        {
+            get { return toltottseg; }
+            set
+            {
+                if ( value <0)
+                {
+                    toltottseg = 0;
+                }
+                else if (value >100)
+                {
+                    toltottseg = 100;
+                }
+            }
+        }
+
+    }
+}
---
+++ TelefonApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using TelefonApp;
+
+Okostelefon nokia = new Okostelefon();
+
+nokia.Modell = "Nokia";
+
+nokia.Toltottseg = -15;
+
+Console.WriteLine(nokia.Modell + " Töltöttség: "+ nokia.Toltottseg);
+
+Okostelefon samsung = new Okostelefon();
+
+samsung.Modell = "Samsung";
+samsung.Toltottseg = 110;
+
+Console.WriteLine(samsung.Modell + " Töltöttség: " +  samsung.Toltottseg);
