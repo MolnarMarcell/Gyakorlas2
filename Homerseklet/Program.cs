955734c70d4e300e6670835e7cf6704192a0a401
MolnarMarcell <marci20070208@gmail.com>
2026-09-17 08:29:28 +0200

    2. feladat

---
+++ Homerseklet/Program.cs
@@ -0,0 +1,13 @@
+﻿using Homerseklet;
+
+HomersekletAtvalto homerseklet = new HomersekletAtvalto();
+
+homerseklet.Celsius = 20;
+
+double Fahrenheit = homerseklet.ToFahrenheit();
+
+Console.WriteLine("Fahrenheit:" + Fahrenheit);
+
+Console.WriteLine("Fahrenheit kapott parameterbol: "+ HomersekletAtvalto.CelsiusToFahrenheit(30));
+
+
