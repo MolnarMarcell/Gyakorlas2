a7484bc64a214a5134b9349fff627a5a1732eab2
MolnarMarcell <marci20070208@gmail.com>
2026-09-17 08:38:10 +0200

    KavezogepApp

---
+++ KavezogepApp/Kavezogep.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KavezogepApp
+{
+    internal class Kavezogep
+    {
+        private int vizKeszletMl;
+
+        public void VizTolt(int ml)
+        {
+            if (ml < 0)
+            {
+                Console.WriteLine("Negatív értéket szeretnél megadni!");
+            }
+            else
+            {
+                vizKeszletMl += ml;
+                Console.WriteLine($"Víz sikeresen betöltve, Érték: {ml} ml");
+            }
+        }
+
+        public void KaveFozes()
+        {
+            if (vizKeszletMl >= 150)
+            {
+                vizKeszletMl -= 150;
+                Console.WriteLine($"Sikeres kávéfőzés!, A vízszint csökkent 150 ml-el, maradék vízszint: {vizKeszletMl} ml");
+            }
+            else
+            {
+                Console.WriteLine("Nincsen elég víz a kávéfőző gépben!");
+            }
+        }
+
+    }
+}
---
+++ KavezogepApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using KavezogepApp;
+
+Kavezogep kavegep = new Kavezogep();
+
+
+//negativ erteknel mi tortenik:
+kavegep.VizTolt(-100);
+
+//pozitiv erteknel mi tortenik:
+kavegep.VizTolt(300);
+
+kavegep.KaveFozes();
