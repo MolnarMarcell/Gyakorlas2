02d4ef8c0db759d1c9cff1e5aa75afcff5ce7cc0
MolnarMarcell <marci20070208@gmail.com>
2026-09-17 09:02:33 +0200

    5. feladat

---
+++ DiakApp/Diak.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DiakApp
+{
+    internal class Diak
+    {
+
+        public string Nev { get; set; }
+        public double Atlag { get; set; }
+
+        public Diak(string nev, double atlag)
+        {
+            this.Nev = nev;
+            this.Atlag = atlag;
+        }
+    }
+}
---
+++ DiakApp/Program.cs
@@ -0,0 +1,7 @@
+﻿using DiakApp;
+
+//Diak kovacspeter = new Diak();
+
+Diak KovacsPeter = new Diak("Kovács Péter", 4.25);
+
+Console.WriteLine(KovacsPeter.Nev + " " + KovacsPeter.Atlag);
